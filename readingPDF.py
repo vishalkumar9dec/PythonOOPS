f7ad1864514751a6faa88fda8fc6edd715a69aa3
vishal.kumar1 <vishalkumar9dec@gmail.com>
2020-03-31 12:03:56 +0530

    Example of reading pdf files in python

---
+++ readingPDF.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfFileReader
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+        print(number_of_pages)
+
+
+extract_information('Resume_data_analyst_experienced.pdf')
